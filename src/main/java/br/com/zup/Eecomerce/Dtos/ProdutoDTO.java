34d7192833f3fafdfeb53862c7f93f899a698259
Raline Silva <ralinesilva1@gmail.com>
2021-10-29 11:47:39 -0300

    Criar classe ProdutoDTO

--- src/main/java/br/com/zup/Eecomerce/Dtos/ProdutoDTO.java
+++ src/main/java/br/com/zup/Eecomerce/Dtos/ProdutoDTO.java
@@ -1,5 +1,31 @@
 package br.com.zup.Eecomerce.Dtos;
 
 public class ProdutoDTO {
+    private String nomeProduto;
+    private double valor;
 
+    public ProdutoDTO() {
+
+    }
+
+    public ProdutoDTO(String nomeProduto, double valor) {
+        this.nomeProduto = nomeProduto;
+        this.valor = valor;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
